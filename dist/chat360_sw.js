202d7cfc1f74993680f4d4c4ca08e90b4634c2e9
Prince <prince@chat360.io>
2025-01-30 11:22:13 +0530

    published dist

---
+++ dist/chat360_sw.js
@@ -0,0 +1,5 @@
+var config = {
+  channelId: "4a23d8be-58a5-4c31-9a3b-41f338aa994f",
+  key: "BD822-dawBIWmO3XHKZo2iho2k9ko9Jqx3MWFSagZQ3mi7tnzr4DFZnHr7FcngpGWkG8rbLJCjPXxcEr5U2mVnw"
+};
+importScripts( "https://staging.chat360.io/push/service_worker/sw.js");
